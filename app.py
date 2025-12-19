df78ba771547cb666f390f886a287811fec118a4
Vinit <vinitkhapekar09@gmail.com>
2025-12-19 18:32:13 +0530

    modified unit commit

--- app.py
+++ app.py
@@ -30,9 +30,9 @@ def predict():
     data = [float(x) for x in request.form.values()]
     final_input = scaler.transform(np.array(data).reshape(1, -1))
     print(final_input)
-    output = model.predict(final_input)[0]
+    output = model.predict(final_input)[0] * 100000
     return render_template(
-        "home.html", prediction_text="The prediction price is {}".format(output)
+        "home.html", prediction_text="The prediction price is ${}".format(output)
     )
 
 
