f88e30561ad089bae8143d9f3119059f532f44eb
Vinit <vinitkhapekar09@gmail.com>
2025-12-19 17:56:52 +0530

    app update commit

--- app.py
+++ app.py
@@ -18,10 +18,22 @@ def home():
 def predict_api():
     data = request.json["data"]
     print(data)
-    print(np.array(list(data.values)).reshape(1, -1))
-    new_data = scaler.transform(np.array(list(data.values)).reshape(1, -1))
+    print(np.array(list(data.values())).reshape(1, -1))
+    new_data = scaler.transform(np.array(list(data.values())).reshape(1, -1))
     output = model.predict(new_data)
     print(output[0])
+    return jsonify(output[0])
+
+
+@app.route("/predict", methods=["POST"])
+def predict():
+    data = [float(x) for x in request.form.values()]
+    final_input = scaler.transform(np.array(data).reshape(1, -1))
+    print(final_input)
+    output = model.predict(final_input)[0]
+    return render_template(
+        "home.html", prediction_text="The prediction price is {}".format(output)
+    )
 
 
 if __name__ == "__main__":
